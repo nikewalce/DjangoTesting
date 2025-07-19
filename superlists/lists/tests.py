b6722dcb5aec724f14b677c0f4f4c36604a92d90
nikewalce <nikewalce1@gmail.com>
2025-07-19 13:40:28 +0300

    fix

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,16 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.view import home_page
 
+class HomePageTest(TestCase):
+    'тест домашней страницы'
+    def test_root_url_resolves_to_home_page_view(self):
+        '''корневой url преобразуется в представление домашней страницы'''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    59 стр
+    
 class SmokeTest(TestCase):
     def test_bad_maths(self):
         self.assertEqual(1+1, 3)
